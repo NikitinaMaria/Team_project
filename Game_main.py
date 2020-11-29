3b99fc5b46ac8131b17eb6133cfa49617d1574f0
NikitinaMaria <71255897+NikitinaMaria@users.noreply.github.com>
2020-11-29 17:07:20 +0300

    Add files via upload

--- Game_main.py
+++ Game_main.py
@@ -12,29 +12,25 @@ FPS = 15
 screen = pygame.display.set_mode((screen_size_x, screen_size_y))
 screen.fill(WHITE)
 
-draw_stats = Draw_stats()
+explanatories = []
+for i in range(3):
+    explanatories.append(Explanatory(i + 1))
 
 edit_events = Editor(250, 6)
 
 clock = pygame.time.Clock()
-done_mechanics = False
-done_stats = False
+done = False
+finished_game = False
 # pygame.mixer.music.load('Ramones_Rock_N_Roll_High_School_1.ogg')
 # pygame.mixer.music.play()
 
-while (not done_mechanics) and (not done_stats):
+while not done:
     clock.tick(FPS)
     pygame.display.update()
     screen.fill(WHITE)
-    done_mechanics = edit_events.process(pygame.event.get())
-    done_stats = draw_stats.draw()
-    if edit_events.hero_boosting():
-        draw_stats.boost_scale.boost_points = 100
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done_mechanics = True
+    done = edit_events.process(pygame.event.get())
 
-if done_mechanics == 2:
-    show_game_over_table(done_mechanics)
+if done == 2:
+    show_game_over_table(done)
 
 pygame.quit()
--- game_mechanics.py
+++ game_mechanics.py
@@ -1,8 +1,12 @@
 from random import randrange as rnd, choice
 from random import randint
+import numpy as np
 import pygame
 from pygame.draw import *
 from game_hero import *
+from game_stats import *
+
+#from Game.game_hero import Hero
 
 pygame.init()
 
@@ -25,6 +29,23 @@ LIL = (190, 0, 255)
 DARK_GREEN = (0, 128, 0)
 GREY = (128, 128, 128)
 
+def insert_picture(name, position, size):
+    '''
+    Paste image according to its position and size which written like (full_size_x, full_size_y)
+    '''
+    surf = pygame.image.load(name)
+    surf = pygame.transform.scale(surf, size)
+    rect = surf.get_rect(center = position)
+    screen.blit(surf, rect)
+
+def insert_text(string, font, color, position, size):
+    '''
+    Paste text according to the position of its center
+    '''
+    text_style = pygame.font.Font(font, size)
+    surface = text_style.render(string, True, color)
+    textRect = surface.get_rect(center = position)
+    screen.blit(surface, textRect)
 
 def quit_condition(pressed_button):
     """
@@ -52,11 +73,7 @@ def show_game_over_table(done):
 	Shows "Game over" table after the hero was smashed by obstacle
 	'''
     screen.fill(GREEN)
-    font = pygame.font.SysFont("dejavusansmono", 100)
-    over_surface = font.render("Game Over", True, WHITE)
-    textRect = over_surface.get_rect()
-    textRect.center = (screen_size[0] // 2, screen_size[1] // 2)
-    screen.blit(over_surface, textRect)
+    insert_text("Game Over", "Game-font.ttf", WHITE, (screen_size[0] // 2, screen_size[1] // 2), 100)
     pygame.display.update()
     while done != 1:
         for event in pygame.event.get():
@@ -121,7 +138,7 @@ class Boost():
 class Editor():
     def __init__(self, width_of_pictures, distance_between_roads):
         '''
-        Set ball's parameters and thier meanings
+        Set parameters and thier meanings
         '''
         self.done = 0
         self.step = (screen_size[0] - 2 * width_of_pictures - distance_between_roads * 2) // 3 + distance_between_roads
@@ -131,6 +148,10 @@ class Editor():
         self.distance_between_roads = distance_between_roads
         self.obstacles = []
         self.boosts = []
+        self.test_is_on = False
+        self.timer = Timer(self)
+        self.mood_scale = Mood_scale(self)
+        self.boost_scale = Boost_scale(self)
 
     def user_events(self, events):
         '''
@@ -139,9 +160,9 @@ class Editor():
         self.done = 0
         for event in events:
             self.done = quit_condition(event.type)
-            if event.type == pygame.KEYUP:
-                self.move_hero(event)
-        return self.done
+            if not self.test_is_on:
+                if event.type == pygame.KEYUP:
+                    self.move_hero(event)
 
     def move_hero(self, event):
         '''
@@ -188,6 +209,9 @@ class Editor():
             obstacle.draw()
         for boost in self.boosts:
             boost.draw()
+        self.mood_scale.draw()
+        self.boost_scale.draw()
+        self.timer.draw()
 
     def check_bumping(self):
         '''
@@ -207,7 +231,7 @@ class Editor():
         for boost in self.boosts:
             if self.hero.coord[1] - self.hero.length <= boost.coord[1] + boost.width:
                 if self.hero.number_of_road == boost.number_of_road:
-		    boost.is_alive = False
+                    boost.is_alive = False
                     return True
 
     def process(self, events):
@@ -215,20 +239,164 @@ class Editor():
 		Manager function for all processes in program. It creates obstacles and control their movement, closes program because of pressed [X] button
 		starts drawing of all process, looks if the hero was kicked
 		'''
-        self.time += 1
-        if self.time % 20 == 0:
-            self.obstacles.append(Obstacle(10, randint(1, 3), 10, self.width_of_pictures, self.distance_between_roads))
-        if self.time % 47 == 0:
-            self.boosts.append(Boost(10, randint(1,3), 70, self.width_of_pictures, self.distance_between_roads))
-        self.done = 0
-        self.done = self.user_events(events)
-        self.draw()
-        self.move_obstacle()
-        self.move_boosts()
-        if self.check_bumping():
-            self.done = 2
+        if not self.test_is_on:
+            self.time += 1
+            if self.time % 20 == 0:
+                self.obstacles.append(Obstacle(10, randint(1, 3), 10, self.width_of_pictures, self.distance_between_roads))
+            if self.time % 47 == 0:
+                self.boosts.append(Boost(10, randint(1,3), 70, self.width_of_pictures, self.distance_between_roads))
+            self.done = 0
+            self.user_events(events)
+            self.draw()
+            self.move_obstacle()
+            self.move_boosts()   
+            if self.hero_boosting():
+                self.boost_scale.boost_points = 100
+            if self.mood_scale.mood_points <= 0:
+                self.done = 1
+            if self.check_bumping():
+                self.done = 2
+            if self.time % 50 == 0:
+                self.test_is_on = True
+                self.Test = Ivanov_test()
+        else:
+            return_list = self.Test.progress(events)
+            self.done = return_list[0]
+            self.test_is_on = return_list[1]
         return self.done
 
+class Ivanov_test():
+    def __init__(self):
+        '''
+        Set test's parameters and thier meanings
+        '''
+        self.time = 0
+        self.length = min(screen_size[0] // 7, screen_size[1] // 7 )
+        self.done = 0
+        self.time_is_over = False
+        self.time_limit = 15 * FPS
+        self.growing_time = 1 * FPS
+        self.x = screen_size[0] // 2
+        self.y = screen_size[1] // 4
+        self.task = []
+        self.length_of_answer_pic = self.length // 3
+        self.create_task()
+        self.number_of_task = 1
+        self.answer_list = []
+        self.score = 0
+        
+    def progress(self, events):
+        '''
+        Manager function for all proccesses in test
+        '''
+        self.time += 1
+        self.draw()
+        self.user_events(events)
+        if self.time == self.time_limit:
+            self.time_is_over = True
+        return (self.done, not self.time_is_over)
+
+    def create_task(self):
+        '''
+        Make the new list with numbers for numers wich mean:
+        1 - for all
+        2 - exist
+        3 - equivalent (then and only then, when)
+        4 - epsilon
+        '''
+        self.task = []
+        for i in range(5):
+            self.task.append(randint(1, 4))
+
+    def draw(self):
+        '''
+        Draws all images and pictures in test
+        '''
+        time_scale = screen_size[0] / self.time_limit
+        rect(screen, RED, (0, 0, int(self.time * time_scale), 10))
+        if self.time <= self.growing_time:
+            insert_text("Dancing time!", "Game-font.ttf", GREEN, (screen_size[0] // 2, screen_size[1] // 2), self.time * self.time)
+        elif self.number_of_task <= 4:
+            rect(screen, BLACK, (self.x - self.length // 2 - 10, self.y - self.length // 2 - 10, self.length + 20, self.length + 20), 5)
+            self.number = self.task[len(self.task) - 1]
+            if self.number == 1:
+                self.draw_for_all()
+            elif self.number == 2:
+                self.draw_exist()
+            elif self.number == 3:
+                self.draw_equivalent()
+            else:
+                self.draw_epsilon() 
+            insert_picture('Hint.png', (4 * screen_size[0] // 5, screen_size[1] // 3), (screen_size[0] // 3, screen_size[1] // 4))   
+            for i in range(4):
+                insert_text(str(i + 1) + ') ', "Game-font.ttf", BLACK, (screen_size[0] // 12, i * self.length_of_answer_pic + screen_size[1] // 5), 20)
+        elif self.number_of_task >= 5:
+            self.time = max(self.time, self.time_limit - (self.growing_time * 2))
+            if self.score == 0:
+                insert_picture('Falure.jpg', (screen_size[0] // 2, screen_size[1] // 2), (screen_size[0], screen_size[1])) 
+                insert_text('You need to work harder', 'Game-font.ttf', WHITE, (screen_size[0] // 2, 3 * screen_size[1] // 4), min(screen_size[0] // 9, screen_size[1] // 9))
+            for i in range(4):
+                insert_text(str(i + 1) + ') ', "Game-font.ttf", WHITE, (screen_size[0] // 12, i * self.length_of_answer_pic + screen_size[1] // 5), 20)
+        self.draw_right_or_wrong_answer()
+        pygame.display.update()
+
+    def draw_right_or_wrong_answer(self):
+        '''
+        Putting pictures of green arrow for right answer or red X for wrong answer
+        '''
+        for i in range(self.number_of_task - 1):
+            if self.answer_list[i] == True:
+                insert_picture('right_ans.png', (screen_size[0] // 12 +  self.length_of_answer_pic, i * self.length_of_answer_pic + screen_size[1] // 5), 
+                              (self.length_of_answer_pic, self.length_of_answer_pic))
+            else:
+                insert_picture('wrong.jpg', (screen_size[0] // 12 +  self.length_of_answer_pic, i * self.length_of_answer_pic + screen_size[1] // 5), 
+                              (self.length_of_answer_pic, self.length_of_answer_pic))
+        
+    def user_events(self, events):
+        '''
+        Analize events from keyboard, mouse, etc.
+        '''
+        for event in events: 
+            self.done = quit_condition(event.type)
+            if (self.time >= self.growing_time) and (event.type == pygame.KEYUP) :
+                if (event.key == pygame.K_d or event.key == pygame.K_RIGHT) and (self.number == 4):
+                    self.task.pop(len(self.task) - 1)
+                elif (event.key == pygame.K_a or event.key == pygame.K_LEFT) and (self.number == 3):
+                    self.task.pop(len(self.task) - 1)
+                elif (event.key == pygame.K_s or event.key == pygame.K_DOWN) and (self.number == 1):
+                    self.task.pop(len(self.task) - 1)
+                elif (event.key == pygame.K_w or event.key == pygame.K_UP) and (self.number == 2):
+                    self.task.pop(len(self.task) - 1)
+                else:
+                    self.answer_list.append(False)
+                    self.create_task()
+                    self.number_of_task += 1
+                if len(self.task) == 0:
+                    self.create_task()
+                    self.answer_list.append(True)
+                    self.number_of_task += 1
+                    self.score += 1
+    
+    def draw_for_all(self):
+        line(screen, BLACK, (self.x - self.length // 2, self.y - self.length // 2), (self.x , self.y + self.length // 2), 5)
+        line(screen, BLACK, (self.x , self.y + self.length // 2), (self.x + self.length // 2, self.y - self.length // 2), 5)
+        line(screen, BLACK, (self.x - self.length // 4, self.y ), (self.x + self.length // 4, self.y ), 5)
+
+    def draw_exist(self):
+        for i in range(3):
+            line(screen, BLACK, (self.x - self.length // 4, self.y - self.length // 2 + (self.length // 2) * i), 
+                (self.x + self.length // 2, self.y - self.length // 2 + (self.length // 2) * i), 5 )
+        line(screen, BLACK, (self.x + self.length // 2, self.y - self.length // 2), (self.x + self.length // 2, self.y + self.length // 2), 5)
+
+    def draw_equivalent(self):
+        line(screen, BLACK, (self.x - self.length // 4, self.y - self.length // 8), (self.x + self.length // 4, self.y - self.length // 8), 5)
+        line(screen, BLACK, (self.x - self.length // 4, self.y + self.length // 8), (self.x + self.length // 4, self.y + self.length // 8), 5)
+        for i in range(-1, 3, 2):
+            line(screen, BLACK, (self.x - i * self.length // 2, self.y), (self.x - i * self.length // 13, self.y - self.length // 4 + 4 ), 5)
+            line(screen, BLACK, (self.x - i * self.length // 2, self.y), (self.x - i * self.length // 13, self.y + self.length // 4 - 4 ), 5)
+    
+    def draw_epsilon(self):
+        insert_picture('epsilon.jpg', (self.x, self.y), (self.length, self.length))
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
