60cc27d93da2e916da86868903f5dd77934c52b6
SomeHuman-exe <71900937+SomeHuman-exe@users.noreply.github.com>
2020-12-07 11:29:31 +0300

    Update Game_main.py

--- Game_main.py
+++ Game_main.py
@@ -13,10 +13,6 @@ FPS = 15
 screen = pygame.display.set_mode((screen_size_x, screen_size_y))
 screen.fill(WHITE)
 
-explanatories = []
-for i in range(3):
-    explanatories.append(Explanatory(i + 1))
-
 pause_menu = Pause(False, screen_size_x, screen_size_y)
 main_menu = Title(True, screen_size_x, screen_size_y)
 choose = Choosing()
