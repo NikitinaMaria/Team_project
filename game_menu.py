bfd85ffdbc2e44a72e9f4b730bc789ebe27682f1
SomeHuman-exe <71900937+SomeHuman-exe@users.noreply.github.com>
2020-12-07 09:42:58 +0300

    Added choosing menu

--- game_menu.py
+++ game_menu.py
@@ -30,12 +30,19 @@ def draw_text(text, font, color, surface, x, y):
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
-hero = Hero()
+
 def preview(gender, x, y):
+    hero = Hero(0, (x, y))
+    hero.size_x = 600
+    hero.size_y = 800
     if gender == 1:
         hero.gender = 1
-        hero.direction = 2
-        hero.draw(1, 100, 100)
+        hero.direction = 0
+        hero.draw()
+    else:
+        hero.gender = 0
+        hero.direction = 0
+        hero.draw()
 
 '''
 pos - Position - (x, y)
@@ -43,9 +50,9 @@ size - Size - (length, width)
 
 Draws button and text
 '''
-def make_button(text, pos, size):
+def make_button(text, pos, size, color):
     button = pygame.Rect(pos[0], pos[1], size[0], size[1])
-    pygame.draw.rect(screen, (255, 0, 0), button)
+    pygame.draw.rect(screen, color, button)
     draw_text(text, font, BLACK, screen, pos[0], pos[1])
     return(button)
 
@@ -60,24 +67,24 @@ class Pause():
 
     def render(self):
         screen.fill(BLACK)
-
         if self.main:
             draw_text('Pause menu', font, WHITE, screen, 20, 20)
 
-            self.button_1 = make_button('Continue', (self.button_size_y, self.button_size_x // 2 ), (self.button_size_x, self.button_size_y))
+            self.button_1 = make_button('Continue', (self.button_size_y, self.button_size_x // 2 ), (self.button_size_x, self.button_size_y), RED)
 
-            self.button_2 = make_button('Options', (self.button_size_y, self.button_size_x ), (self.button_size_x, self.button_size_y))
+            self.button_2 = make_button('Options', (self.button_size_y, self.button_size_x ), (self.button_size_x, self.button_size_y), RED)
 
-            self.button_3 = make_button('Quit', (self.button_size_y, (3*self.button_size_x // 2) ), (self.button_size_x, self.button_size_y))
+            self.button_3 = make_button('Quit', (self.button_size_y, (3*self.button_size_x // 2) ), (self.button_size_x, self.button_size_y), RED)
 
         if self.options:
             draw_text('Options', font, WHITE, screen, 20, 20)
 
-            self.button_1 = make_button('Option #1', (self.button_size_y, self.button_size_x // 2 ), (self.button_size_x, self.button_size_y))
+            self.button_1 = make_button('Option #1', (self.button_size_y, self.button_size_x // 2 ), (self.button_size_x, self.button_size_y), RED)
 
-            self.button_2 = make_button('Option #2', (self.button_size_y, self.button_size_x ), (self.button_size_x, self.button_size_y))
+            self.button_2 = make_button('Option #2', (self.button_size_y, self.button_size_x ), (self.button_size_x, self.button_size_y), RED)
 
-            self.button_3 = make_button('Back', (self.button_size_y, (3*self.button_size_x // 2)), (self.button_size_x, self.button_size_y))
+            self.button_3 = make_button('Back', (self.button_size_y, (3*self.button_size_x // 2)), (self.button_size_x, self.button_size_y), RED)
+
 
     def check(self, events):
         mouse_x, mouse_y = pygame.mouse.get_pos(events)
@@ -138,20 +145,20 @@ class Title():
         if self.main:
             draw_text('Main menu', font, WHITE, screen, 20, 20)
 
-            self.button_1 = make_button('Start !', (self.button_size_y, self.button_size_x // 2 ), (self.button_size_x, self.button_size_y))
+            self.button_1 = make_button('Start !', (self.button_size_y, self.button_size_x // 2 ), (self.button_size_x, self.button_size_y), RED)
 
-            self.button_2 = make_button('Options', (self.button_size_y, self.button_size_x ), (self.button_size_x, self.button_size_y))
+            self.button_2 = make_button('Options', (self.button_size_y, self.button_size_x ), (self.button_size_x, self.button_size_y), RED)
 
-            self.button_3 = make_button('Quit', (self.button_size_y, (3*self.button_size_x // 2) ), (self.button_size_x, self.button_size_y))
+            self.button_3 = make_button('Quit', (self.button_size_y, (3*self.button_size_x // 2) ), (self.button_size_x, self.button_size_y), RED)
 
         if self.options:
             draw_text('Options', font, WHITE, screen, 20, 20)
 
-            self.button_1 = make_button('Option #1', (self.button_size_y, self.button_size_x // 2 ), (self.button_size_x, self.button_size_y))
+            self.button_1 = make_button('Option #1', (self.button_size_y, self.button_size_x // 2 ), (self.button_size_x, self.button_size_y), RED)
 
-            self.button_2 = make_button('Option #2', (self.button_size_y, self.button_size_x ), (self.button_size_x, self.button_size_y))
+            self.button_2 = make_button('Option #2', (self.button_size_y, self.button_size_x ), (self.button_size_x, self.button_size_y), RED)
 
-            self.button_3 = make_button('Back', (self.button_size_y, (3*self.button_size_x // 2) ), (self.button_size_x, self.button_size_y))
+            self.button_3 = make_button('Back', (self.button_size_y, (3*self.button_size_x // 2) ), (self.button_size_x, self.button_size_y), RED)
 
     def check(self, events):
         mouse_x, mouse_y = pygame.mouse.get_pos(events)
@@ -192,3 +199,42 @@ class Title():
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
+
+class Choosing:
+    def __init__(self):
+        self.main = True
+        self.gender = 1
+        self.button_size_x = 200
+        self.button_size_y = 200
+
+    def render(self):
+        screen.fill(BLACK)
+        preview(1, 100, 100)
+        preview(0, 800, 100)
+        draw_text('Welcome to the MIPT, buddy', font, WHITE, screen, 400, 50)
+        draw_text('Choose your warrior', font, WHITE, screen, 400, 100)
+        self.button_1 = pygame.Rect(100, 100, self.button_size_x, self.button_size_y)
+        self.button_2 = pygame.Rect(800, 100, self.button_size_x, self.button_size_y)
+        self.button_3 = make_button('Oh no, pls let me go ;C', (400, 500), (150, 50), BLUE)
+
+    def check(self, events):
+        mouse_x, mouse_y = pygame.mouse.get_pos(events)
+        for event in events:
+
+            if self.main:
+                if self.button_1.collidepoint((mouse_x, mouse_y)) and (event.type == MOUSEBUTTONDOWN) and (event.button == 1):
+                    self.main = False
+                    self.gender = 1
+
+                if self.button_2.collidepoint((mouse_x, mouse_y)) and (event.type == MOUSEBUTTONDOWN) and (event.button == 1):
+                    self.main = False
+                    self.gender = 0
+
+                if self.button_3.collidepoint((mouse_x, mouse_y)) and (event.type == MOUSEBUTTONDOWN) and (event.button == 1):
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
